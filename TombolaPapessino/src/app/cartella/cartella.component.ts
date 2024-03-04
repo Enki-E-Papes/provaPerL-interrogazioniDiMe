6658e40da4c5a206cc39b6a6bccf4c607fb7393c
Lich-senpai13 <papesenki.enrico@iisgalvanimi.edu.it>
2024-03-04 10:58:43 +0000

    comodo34

---
+++ TombolaPapessino/src/app/cartella/cartella.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cartella',
+  templateUrl: './cartella.component.html',
+  styleUrls: ['./cartella.component.css']
+})
+export class CartellaComponent {
+
+}
