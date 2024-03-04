811beaeb5e7288061bbe681f4f914e2ac7b83d70
Lich-senpai13 <papesenki.enrico@iisgalvanimi.edu.it>
2024-03-04 11:33:28 +0000

    pushanlo

--- TombolaPapessino/src/app/app.component.ts
+++ TombolaPapessino/src/app/app.component.ts
@@ -7,13 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   numerazzo : number;
+  vetto : number[]; // crea l'array 
 
   constructor(){
     this.numerazzo = 0;
+    this.vetto = [];
   }
   cliccanza(N:HTMLInputElement){
     this.numerazzo = Number(N.value)
     console.log(this.numerazzo);
+    this.vetto.push(this.numerazzo);
+    console.log(this.vetto)
     return false
   }
 }
